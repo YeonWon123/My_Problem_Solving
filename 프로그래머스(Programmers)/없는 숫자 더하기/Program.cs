8cc5c3c117d43836bee8f54869b7375091fc2cab
남윤원 <weonwon123@naver.com>
2021-11-25 21:09:06 +0900

    2021-11-25 없는 숫자 더하기 C# 풀이

---
+++ 프로그래머스(Programmers)/없는 숫자 더하기/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        static int[] check = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        static int solution(int[] numbers)
+        {
+
+            int answer = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                check[numbers[i]]++;
+            }
+
+            for (int i = 0; i < check.Length; i++)
+            {
+                if (check[i] == 0)
+                {
+                    answer += i;
+                }
+            }
+
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3 };
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Program.solution(numbers));
+        }
+    }
+}
